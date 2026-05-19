373cb091ca8bcbd361d579b93da37f15c8a480be
1-samarth <samarthchandel587@gmail.com>
2026-05-19 20:17:24 +0530

    fix entity scanning

--- backend/src/main/java/com/samarth/taskmanager/TeamTaskManagerApplication.java
+++ backend/src/main/java/com/samarth/taskmanager/TeamTaskManagerApplication.java
@@ -2,9 +2,14 @@ package com.samarth.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.samarth.taskmanager.model")
+@EnableJpaRepositories(basePackages = "com.samarth.taskmanager.repository")
 public class TeamTaskManagerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TeamTaskManagerApplication.class, args);
     }
